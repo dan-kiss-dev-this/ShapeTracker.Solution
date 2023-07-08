e358a1f3570f9033af16ca28c20ec3abec83c72b
Daniel Kiss <danielkiss@MacBook-Pro.lan>
2023-07-07 18:46:55 -0700

    add more models

---
+++ ShapeTracker/UserInterfaceModels/Banners.cs
@@ -0,0 +1,26 @@
+namespace ShapeTracker.UserInterfaceModels
+{
+    class WelcomeBanner
+    {
+        public static string Welcome { get; } = @"
+  #####                                #######                                           
+ #     # #    #   ##   #####  ######      #    #####    ##    ####  #    # ###### #####  
+ #       #    #  #  #  #    # #           #    #    #  #  #  #    # #   #  #      #    # 
+  #####  ###### #    # #    # #####       #    #    # #    # #      ####   #####  #    # 
+       # #    # ###### #####  #           #    #####  ###### #      #  #   #      #####  
+ #     # #    # #    # #      #           #    #   #  #    # #    # #   #  #      #   #  
+  #####  #    # #    # #      ######      #    #    # #    #  ####  #    # ###### #    # 
+  ";
+    }
+
+    class GoodbyeBanner
+    {
+        // GoodbyeBanner code here.
+    }
+
+    class SubMenuBanner
+    {
+        // SubMenuBanner code here.
+    }
+
+}
