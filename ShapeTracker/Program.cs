16894e79476fe1a7f6510e35522cce123a1956b4
Daniel Kiss <danielkiss@MacBook-Pro.lan>
2023-07-07 17:30:51 -0700

    change program to loop as many times as wanted via invoking Main() at end if user wants

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using ShapeTracker.Models; // called a directive
-using System.Collections.Generic;
 
 namespace ShapeTracker
 {
@@ -8,27 +7,70 @@ namespace ShapeTracker
     {
         static void Main()
         {
-            List<Triangle> allTriangles = Triangle.GetAll();
-            // we will add user interface code here
-            Triangle testTriangle = new Triangle(4, 4, 4);
-            Triangle secondTriangle = new Triangle(32, 74, 75);
+            Console.WriteLine("Welcome to the Shape Tracker app!");
+            Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
+            Console.WriteLine("Enter a number for side 1");
+            string stringNumber1 = Console.ReadLine();
+            Console.WriteLine("Enter a number for side 2");
+            string stringNumber2 = Console.ReadLine();
+            Console.WriteLine("Enter a number for side 3");
+            string stringNumber3 = Console.ReadLine();
+            int length1 = int.Parse(stringNumber1);
+            int length2 = int.Parse(stringNumber2);
+            int length3 = int.Parse(stringNumber3);
+            Triangle tri = new Triangle(length1, length2, length3);
+            ConfirmOrEditTriangle(tri);
 
-            if (allTriangles.Count == 0)
+            static void ConfirmOrEditTriangle(Triangle tri)
             {
-                Console.WriteLine("There are no triangles");
+                Console.WriteLine("Please confirm that you entered in your triangle correctly:");
+                Console.WriteLine($"Side 1 has a length of {tri.GetSide1()}.");
+                Console.WriteLine($"Side 2 has a length of {tri.GetSide2()}.");
+                Console.WriteLine($"Side 3 has a length of {tri.GetSide3()}.");
+                Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter the triangle's sides");
+                string userInput = Console.ReadLine();
+                if (userInput == "yes")
+                {
+                    // We have not created the CheckTriangleType() UI method yet.
+                    CheckTriangleType(tri);
+                }
+                else
+                {
+                    Console.WriteLine("Let's fix your triangle. Please enter the 3 sides again!");
+                    Console.WriteLine("Please enter a number:");
+                    string stringNumber1 = Console.ReadLine();
+                    Console.WriteLine("Enter another number:");
+                    string stringNumber2 = Console.ReadLine();
+                    Console.WriteLine("Enter a third number:");
+                    string stringNumber3 = Console.ReadLine();
+                    tri.SetSide1(int.Parse(stringNumber1));
+                    tri.SetSide2(int.Parse(stringNumber2));
+                    tri.SetSide3(int.Parse(stringNumber3));
+                    ConfirmOrEditTriangle(tri);
+                }
+
+            }
+        }
+
+        static void CheckTriangleType(Triangle tri)
+        {
+            string result = tri.CheckType();
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Your result is: " + result + ".");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("What's next?");
+            Console.WriteLine("Would you like to check a new triangle (new)?");
+            Console.WriteLine("Please enter 'new' to check the type of a new triangle. To exit, enter any key.");
+            string userResponse = Console.ReadLine();
+            if (userResponse == "new" || userResponse == "New")
+            {
+                Main();
             }
             else
             {
-                Console.WriteLine("------------------");
-                foreach (Triangle tri in allTriangles)
-                {
-                    Console.WriteLine($"Side 1 of the triangle: {tri.GetSide1()}");
-                    Console.WriteLine($"Side 2 of the triangle: {tri.GetSide2()}");
-                    Console.WriteLine($"Side 3 of the triangle: {tri.GetSide3()}");
-                    Console.WriteLine("------------------");
-                }
+                Console.WriteLine("Goodbye!");
             }
         }
-    }
 
+    }
 }
